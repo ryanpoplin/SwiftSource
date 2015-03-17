e102a86e59dcc66d1bc5bb38b559da449c10569f
Byrdann Fox <mobileupstate@gmail.com>
2015-03-17 02:11:00 -0400

    Updates...

--- testplayground.playground/section-1.swift
+++ testplayground.playground/section-1.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
+//// perfect for algorithms...
+//var x = 10
+//for index in 1...20 {
+//    let y = index * x--
+//}
+//
+//var i: Int
+//for i = 0; i <= 24; ++i {
+//    let j = i
+//}
+//
+//let testLabel = UILabel(frame: CGRectMake(0, 0, 200, 50))
+//testLabel.backgroundColor = UIColor.redColor()
+//testLabel.text = "Quasar Blu"
+//testLabel.textAlignment = .Center
+//testLabel.font = UIFont(name: "Sans serif", size: 24)
+//testLabel
+//
+//var testNumber = 24
+//var testLetter = "R"
+//println("Int32 Min = \(Int32.min) Int32 Max = \(Int32.max)")
+
